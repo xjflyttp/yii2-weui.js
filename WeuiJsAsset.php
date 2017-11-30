a9e93d428e79c3e3e2468dab67f145ebe14cdd2f
xjflyttp <xjflyttp@gmail.com>
2017-11-30 11:29:11 +0800

    fix basePath

--- WeuiJsAsset.php
+++ WeuiJsAsset.php
@@ -11,7 +11,6 @@ class WeuiJsAsset extends AssetBundle
 {
 
     public $sourcePath = '@npm/weui.js/dist';
-    public $basePath = '@webroot/assets';
     public $js = ['weui.js'];
     public $depends = ['yii\web\JqueryAsset'];
 
